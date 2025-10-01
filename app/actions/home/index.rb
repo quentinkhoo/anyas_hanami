e8d44c07a4b626075a49f18e72142070abcf6339
Quentin Khoo <quentin.khoo@ascenda.com>
2025-10-01 19:04:55 +0800

    remove comments

--- app/actions/home/index.rb
+++ app/actions/home/index.rb
@@ -11,16 +11,7 @@ module AnyasHanami
           output = ""
 
           if request.post?
-            # 1. Get the sanitized input from the form params
             user_input = request.params[:user_input].to_s.strip
-
-            # # 2. Define the safe, unchangeable command we will run
-            # command = "ls"
-            # args = ["-la", user_input] # The user input is ONLY an argument
-
-            # # 3. Safely execute the command
-            # # Open3 prevents shell injection by separating the command from arguments
-            # stdout, stderr, status = Open3.capture3(command, *args)
 
             output = Utils::SafeCommand.process(user_input)
           end
