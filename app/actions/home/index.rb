3bee7b063da363ef66a64d2915bcd656d31cb0e0
Quentin Khoo <quentin.khoo@ascenda.com>
2025-10-02 15:29:26 +0800

    cute af

--- app/actions/home/index.rb
+++ app/actions/home/index.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-require "safe_ruby"
 require "utils/safe_command"
 
 module AnyasHanami
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ module AnyasHanami
   class Routes < Hanami::Routes
     # Add your routes here. See https://guides.hanamirb.org/routing/overview/ for details.
     root to: "home.index"
-    post "/", to: "home.index", as: :home_create
+    post "/", to: "home.index"
   end
 end
